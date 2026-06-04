249271c4ece231e358c228f40219481d02656d83
Yusuf Kemer <ykemer694@gmail.com>
2026-06-04 19:17:44 +0300

    pwa service worker aktif

--- app/next.config.js
+++ app/next.config.js
@@ -2,9 +2,11 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: false,
 });
 
-module.exports = withPWA({
+const nextConfig = {
   reactStrictMode: true,
-});
+};
+
+module.exports = withPWA(nextConfig);
